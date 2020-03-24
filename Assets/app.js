aa3449c3343fbf1b6d7a849509a9faf640c1c061
Roger <rmartinez5926.rm@gmail.com>
2020-03-24 15:17:22 -0400

    put to articles on the page with links to the articles

--- Assets/app.js
+++ Assets/app.js
@@ -14,7 +14,7 @@ function coronaAjax() {
             "x-rapidapi-key": "e4420810e6msh08ee16571aa2e63p11d2e6jsnc7f841440ab7"
         }
     }
-    
+
     $.ajax(settings).done(function (response) {
         console.log(JSON.parse(response));
     });
@@ -28,7 +28,7 @@ function newsApiCall(countryCode) {
     var newsAPIKey = "7df80f17ac7a4d13ae60f8308308d6f1";
     //URL to get response from
     var queryURL = "http://newsapi.org/v2/top-headlines?country=" + countryCode + "&category=general&apiKey=" + newsAPIKey;
-//ajax call to news API to get data
+    //ajax call to news API to get data
     $.ajax({
         url: queryURL,
         method: "GET"
@@ -37,29 +37,36 @@ function newsApiCall(countryCode) {
         console.log(response);
         //target empty div to append to
         var newsDiv1 = $("#news1");
-        //get a random article from array
-        var newsIndex = Math.floor(Math.random()*response.articles.length);
-        console.log(newsIndex)
-        //get the title
-        var newsTitle = response.articles[newsIndex].title;
-        //put title in h3
-        var titleHeader = $("<h3>").text(newsTitle);
-        //get author
-        var newsAuthor = response.articles[newsIndex].author
-        //put author name in a p
-        var authorP = $("<p>").text(newsAuthor);
-        //create image element
-        var newsImage = $("<img>");
-        //get image url and article url
-        newsImage.attr("src", response.articles[newsIndex].urlToImage).attr("href", response.articles[newsIndex].url);
-        //get description
-        var newsDescription = response.articles[newsIndex].description;
-        //put description in a p
-        var descriptionP = $("<p>").text(newsDescription);
-        newsDiv1.append(titleHeader,authorP,newsImage, descriptionP);
+        //empty div before appending new articles
+        newsDiv1.empty();
+        //make loop to put 2 random articles on the page 
+        for (let index = 0; index < 2; index++) {
+            //get a random article from array
+            var newsIndex = Math.floor(Math.random() * response.articles.length);
+            //get the title
+            var newsTitle = response.articles[newsIndex].title;
+            //put title in h3
+            var titleHeader = $("<h3>").text(newsTitle);
+            //get author
+            var newsAuthor = response.articles[newsIndex].author
+            //put author name in a p
+            var authorP = $("<p>").text("Author: " + newsAuthor);
+            //create image element
+            var newsImage = $("<img>").attr("id", "article-img" + index);
+            //get image url 
+            newsImage.attr("src", response.articles[newsIndex].urlToImage);
+            //get description
+            var newsDescription = response.articles[newsIndex].description;
+            //put description in a p
+            var descriptionP = $("<p>").text("Description: " + newsDescription);
+            //append empty div on the page to put article
+            newsDiv1.append(titleHeader, authorP, newsImage, descriptionP);
+            //add a link to the news article
+            $("#article-img" + index).wrap("<a href=" + response.articles[newsIndex].url + " target=\"_blank\"></a>");
+        }
 
     })
 }
 //test news API function
-newsApiCall("us");
+// newsApiCall("us");
 
