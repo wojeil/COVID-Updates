609aea28f2ae2a03ca4d97496fcf52c8b4889ad6
Roger <rmartinez5926.rm@gmail.com>
2020-03-24 14:30:29 -0400

    funtion to append news article to page

--- Assets/app.js
+++ Assets/app.js
@@ -22,19 +22,42 @@ function coronaAjax() {
 
 coronaAjax();
 
-//function
+//function to get news response and append to page
 function newsApiCall(countryCode) {
     //Key to access API
     var newsAPIKey = "7df80f17ac7a4d13ae60f8308308d6f1";
     //URL to get response from
-    var queryURL = "http://newsapi.org/v2/top-headlines?country=" + countryCode + "&q=coronavirus&apiKey=" + newsAPIKey;
+    var queryURL = "http://newsapi.org/v2/top-headlines?country=" + countryCode + "&category=general&apiKey=" + newsAPIKey;
 //ajax call to news API to get data
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function (response) {
         //console log the response object
-        console.log(response)
+        console.log(response);
+        //target empty div to append to
+        var newsDiv1 = $("#news1");
+        //get a random article from array
+        var newsIndex = Math.floor(Math.random()*response.articles.length);
+        console.log(newsIndex)
+        //get the title
+        var newsTitle = response.articles[newsIndex].title;
+        //put title in h3
+        var titleHeader = $("<h3>").text(newsTitle);
+        //get author
+        var newsAuthor = response.articles[newsIndex].author
+        //put author name in a p
+        var authorP = $("<p>").text(newsAuthor);
+        //create image element
+        var newsImage = $("<img>");
+        //get image url and article url
+        newsImage.attr("src", response.articles[newsIndex].urlToImage).attr("href", response.articles[newsIndex].url);
+        //get description
+        var newsDescription = response.articles[newsIndex].description;
+        //put description in a p
+        var descriptionP = $("<p>").text(newsDescription);
+        newsDiv1.append(titleHeader,authorP,newsImage, descriptionP);
+
     })
 }
 //test news API function
