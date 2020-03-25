3e03094ac27289ff1f99e9b671b9a0c3346904dd
Wadih Ojeil <wadih.ojeil@gmail.com>
2020-03-25 00:39:56 -0400

    did some minor tweeks

--- Assets/app.js
+++ Assets/app.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
 //reach for dropdown button
     $("#searchTab").on("click",function(){
         $("#drop").css("display","inline");
-    dropItems();
+    
 });
 
 
@@ -22,37 +22,39 @@ $(document).ready(function () {
 //Targeting our countries
 
 $(".dropdown-item").on("click",function(){
+//set country ID for News API
     var countryID= $(this).data("country");
+//set country Name for Corona API
     var country= $(this).text();
 
 
     alert(countryID + country);
-
-
+    function coronaAjax() {
+            
+        var settings = {
+            "async": true,
+            "crossDomain": true,
+            "url": `https://coronavirus-monitor.p.rapidapi.com/coronavirus/latest_stat_by_country.php?country=${country}`,
+            "method": "GET",
+            "headers": {
+                "x-rapidapi-host": "coronavirus-monitor.p.rapidapi.com",
+                "x-rapidapi-key": "e4420810e6msh08ee16571aa2e63p11d2e6jsnc7f841440ab7"
+            }
+        }
+    
+        $.ajax(settings).done(function (response) {
+            console.log(JSON.parse(response));
+        });
+    }
+    
+    
+    coronaAjax();
 })
 
 
 
 // Corona function
-function coronaAjax() {
-    var country = "spain";
-    var settings = {
-        "async": true,
-        "crossDomain": true,
-        "url": `https://coronavirus-monitor.p.rapidapi.com/coronavirus/latest_stat_by_country.php?country=${country}`,
-        "method": "GET",
-        "headers": {
-            "x-rapidapi-host": "coronavirus-monitor.p.rapidapi.com",
-            "x-rapidapi-key": "e4420810e6msh08ee16571aa2e63p11d2e6jsnc7f841440ab7"
-        }
-    }
 
-    $.ajax(settings).done(function (response) {
-        console.log(JSON.parse(response));
-    });
-}
-
-coronaAjax();
 
 //function to get news response and append to page
 function newsApiCall(countryCode) {
@@ -123,7 +125,7 @@ var settings = {
 
 })
 
-$.ajax(settings).done(function (response) {
-	console.log(response);
-});
+// $ajax(settings).done(function (response) {
+// 	console.log(response);
+// });
 
